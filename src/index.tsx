28ee9580760a9eae7c53abc40f561c07cf0219bb
Yan <yan@yanjobs.me>
2022-10-17 14:35:39 +0200

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,50 @@
+import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { React } from 'enmity/metro/common';
+import { getByProps } from 'enmity/metro';
+import { create } from 'enmity/patcher';
+import manifest from '../manifest.json';
+import { ApplicationCommandInputType, 
+    ApplicationCommandOptionType, 
+    ApplicationCommandType, 
+    Command 
+} from "enmity/api/commands";
+import { sendReply } from "enmity/api/clyde";
+
+const Bitches: Plugin = {
+   ...manifest,
+
+   onStart() {
+     const bitch: Command = {
+  id: "motivation-command",
+  name: "bitches",
+  displayName: "bitches",
+  description: "Gives you bitches",
+  displayDescription: "Gives you bitches",
+      
+  type: ApplicationCommandType.Chat,
+  inputType: ApplicationCommandInputType.BuiltIn,
+
+  execute: async function (args, message) : Promise<void> {
+    const channelId = message.channel.id;
+    const datingLinks = [
+      'https://tinder.com',
+      'https://match.com',
+      'https://dating.com',
+      'https://letmedate.com'
+    ];
+    if (Math.floor(Math.random() * 2) === 1) {
+      sendReply(channelId, "https://imgflip.com/i/6x72vy")
+    } else {
+      sendReply(channelId, 'Click here to get bitches: ' + datingLinks[Math.floor(Math.random()*datingLinks.length)])
+    }
+  }
+};
+     this.commands = [bitch]
+   },
+
+   onStop() {
+     this.commands = []
+   },
+};
+
+registerPlugin(Bitches);
